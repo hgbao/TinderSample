527186e92b212873b07af9fa72b976118379ac64
Bao Huynh <bao.huynh@christinas.vn>
2019-11-13 21:08:39 +0700

    [Application] Initialize theme resource

---
+++ src/theme/colors.js
@@ -0,0 +1,18 @@
+const app = {
+  background: '#FFFFFF',
+  backgroundSecondary: '#F9F9F9',
+  active: '#9FB464',
+  inactive: '#D9D9D9',
+  border: '#2D2E32',
+  shadow: '#454647'
+};
+
+const text = {
+  textPrimary: '#2c2e31',
+  textSecondary: '#999999'
+};
+
+export default {
+  ...app,
+  ...text
+};
